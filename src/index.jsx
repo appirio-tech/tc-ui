9e35ab88fa34175393db15241c4686b5d1e2f465
Nick Litwin <nlitwin@appirio.com>
2016-04-13 17:26:15 -0700

    Remove unused var

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,5 @@
 import React          from 'react'
 import { render }     from 'react-dom'
-import { Provider }   from 'react-redux'
 import browserHistory from 'react-router/lib/browserHistory'
 import Router         from 'react-router/lib/Router'
 
