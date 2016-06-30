41e0edaa079146671ea8465fd607e885e647a36f
vikasrohit <vikas.agarwal@appirio.com>
2016-06-30 14:51:27 +0530

    AS#149564193179843, Import and render Navbar from react-components repo -- fixed lint errors

--- exports.js
+++ exports.js
@@ -1,12 +1,11 @@
-var path = require('path');
-var stylesDir = __dirname + '/src/styles'
+const stylesDir = __dirname + '/src/styles'
 
 function includePaths() {
-  return require('node-neat').includePaths.concat(stylesDir);
+  return require('node-neat').includePaths.concat(stylesDir)
 }
 
 module.exports = {
 
   includePaths: includePaths()
 
-};
+}
