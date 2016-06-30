417d33906f27c08495ee4974bfb40cbbeff09baf
vikasrohit <vikas.agarwal@appirio.com>
2016-06-30 12:59:11 +0530

    AS#149564193179843, Import and render Navbar from react-components repo -- Updating style export so that import can be more straight forward and don't reveal the actual source code location

---
+++ exports.js
@@ -0,0 +1,12 @@
+var path = require('path');
+var stylesDir = __dirname + 'src/styles'
+
+function includePaths() {
+  return require('node-neat').includePaths.concat(stylesDir);
+}
+
+module.exports = {
+
+  includePaths: includePaths()
+
+};
